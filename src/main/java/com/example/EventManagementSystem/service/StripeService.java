e7e1d994d97c5deee2e8dff8c8972392a7983c75
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-08-02 20:33:47 +0200

    .

--- src/main/java/com/example/EventManagementSystem/service/StripeService.java
+++ src/main/java/com/example/EventManagementSystem/service/StripeService.java
@@ -4,13 +4,19 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StripeService {
 
-    public StripeService(){
-        Stripe.apiKey = "sk_test_51QT0bRQ5QqlsljTLoo7zNl3HnBytP43n0njw0ipTarT0DSvTzVINJ0iEUtmFhuYTbl4VQxzP6QGfalvDlndgs0G500sBdDcxoS";
+    @Value("${stripe.secret.key}")
+    private String stripeSecretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = stripeSecretKey;
     }
 
     public String createPaymentIntent(int amount, String currency) throws StripeException{
